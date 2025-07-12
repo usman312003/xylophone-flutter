c75b0bb88b81fe759a7df6a81772ba96cd105f28
Usman <22jzbcs0205@uetpeshawar.edu.pk>
2025-07-12 11:30:53 +0500

    color changed

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,11 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.blueGrey, soundNumber: 2),
               buildKey(color: Colors.yellow, soundNumber: 3),
               buildKey(color: Colors.green, soundNumber: 4),
               buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.brown, soundNumber: 6),
               buildKey(color: Colors.purple, soundNumber: 7),
             ],
           ),
